e35097ccf38ce1baf232aa29f2d12cc35407e9dc
Urs <urs.hurni@unil.ch>
2024-03-17 18:15:22 +0100

    setup

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def home():
 
 @app.route('/energizing-change')
 def project1():
-    return render_template('energizing_change/report.html')
+    return render_template('energizing_change/energizing_change.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
